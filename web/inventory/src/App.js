e127c81227458ab4b3a3c3576bab400284153c50
Chaitya <chaityavora02@gmail.com>
2023-04-01 20:36:36 +0530

    navbar added

--- web/inventory/src/App.js
+++ web/inventory/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Body from './components/Error/Error';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       
     <BrowserRouter>
  
-   
+        <Navbar/>
         <Routes>
         <Route exact path='*' element={<Body/>} />
         </Routes>
