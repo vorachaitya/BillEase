ed1ba20842e7fa81b70c8988d096bd34480017fe
Chaitya <chaityavora02@gmail.com>
2023-04-01 19:23:15 +0530

    footer created

--- web/inventory/src/App.js
+++ web/inventory/src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Body from './components/Error/Error';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+      
+    <BrowserRouter>
+ 
+   
+        <Routes>
+        <Route exact path='*' element={<Body/>} />
+        </Routes>
+        <Footer/>
+        </BrowserRouter>
+    </>
   );
 }
 
